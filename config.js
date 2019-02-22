16b9f4b82129ed393f5c175386de42cf7bc530f3
wpittman <wpittman@paypal.com>
2019-02-22 14:13:59 -0600

    completed, credentials removed

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 module.exports = {
   nvpCreds: {
-    user: "wpittman_api1.paypal.com",
-    pwd: "C7CM265TPAMF2LM7",
-    signature: "AFcWxV21C7fd0v3bYYYRCpSSRl31AART2vD.JQ4wJAUVJrAyxLH3IrN3"
+    user: "your user",
+    pwd: "your pwd",
+    signature: "your signature"
   },
   restCreds: {
-    client_id:
-      "Af_OpabuECsNfU6AzLf1PT213b3BGAy6CRJa5IEr3hteYlOFobgeo0D9gTXxgH4l4UjyhJplVW2xaGVO",
-    client_secret:
-      "EIcCShuD64_P_poFIfldFS9xj7t_wfKDTSzWCaOy8y8JUtsZ9A3pp9fcgPfWcqiUWw01BOPlz_6DWpUt"
+    client_id: "your client_id",
+    client_secret: "your client_secret"
   }
 };
